6ae8933aca761b32d440dd061cc0b466be5d30ab
John Omotani <john.omotani@ukaea.uk>
2023-01-09 17:16:20 +0000

    Fix and update examples, add CI check (#15)
    * Fix and update examples
    
    * Add example of surface plot to elm-pb example
    
    * Github Action to test examples
    
    * Add `apt-get update` to CI job to prevent errors
    
    * Update CI to test on all supported Python versions
    
    ...currently 3.8 to 3.11
    
    * Example script creating a 3d animation for elm-pb
    
    * In CI, test Python scripts before Jupyter notebooks
    
    The tests of python scripts must come first, because the jupyter
    notebook tests work by converting the notebook to a Python script, and
    we don't want to test those scripts as they will fail due to iPython
    stuff not being imported.
    
    * Fix paths in elm-pb/3d-movie.py
    
    Previously script would fail if it was run from anywhere other than the
    elm-pb directory.
    
    * Download elm-pb data if it does not already exist in elm-pb/3d-movie.py
    
    * CI work around for mayavi failing to build on Python-3.11
    
    Skip installing mayavi and running the */*.py scripts for the moment,
    because mayavi fails to build on Python-3.11 (and the only script that
    exists at the moment uses mayavi).
    
    * Skip 3d plot script in CI for now
    
    ...need to figure out how to get mayavi to run in headless mode on the
    CI server.

---
+++ elm-pb/3d-movie.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+"""
+Example producing a 3d animation from an elm-pb simulation.
+
+For viewing the `.gif` produced by this script, you might find the [multigifview
+viewer](https://github.com/johnomotani/multigifview) helpful.
+"""
+
+import dask
+from pathlib import Path
+import urllib.request
+from xbout import open_boutdataset
+
+script_dir = Path(__file__, "..").resolve()
+
+datapath = script_dir.joinpath("BOUT.dmp.*.nc")
+gridpath = script_dir.joinpath("cbm18_dens8.grid_nx68ny64.nc")
+variable = "P"
+save_as = script_dir.joinpath(f"{variable}.gif")
+
+# Download data files if you do not have them already
+for filename in ["cbm18_dens8.grid_nx68ny64.nc", "BOUT.dmp.0.nc"]:
+    if not Path(filename).exists():
+        savepath = script_dir.joinpath(filename)
+        urllib.request.urlretrieve(
+            f"https://zenodo.org/record/4295926/files/{filename}?download=1", savepath
+        )
+
+ds = open_boutdataset(
+    datapath,
+    gridfilepath=gridpath,
+    geometry="toroidal",
+    keep_xboundaries=False,
+    keep_yboundaries=False,
+    info=False,
+    chunks={"t": 1},
+)
+
+nx = ds.metadata["nx"]
+
+da = ds[variable].bout.interpolate_parallel(n=16).bout.from_field_aligned()
+
+da.bout.plot3d(
+    engine="mayavi",
+    style="surface",
+    colormap="viridis",
+    colorbar={"orientation": "vertical", "title": variable},
+    colorbar_font_size=16,
+    vmin=None,
+    vmax=None,
+    mayavi_figure_args={
+        "size": (800, 800),
+        "bgcolor": (1.0, 1.0, 1.0),
+        "fgcolor": (0.0, 0.0, 0.0),
+    },
+    mayavi_view=(-40, 80, 8),
+    surface_xinds=(nx // 3, 38),
+    save_as=save_as,
+)
