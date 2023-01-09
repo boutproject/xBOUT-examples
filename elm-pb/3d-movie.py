26bb14ac3edddebbf5866c9f70dc9b85c30de5aa
John Omotani <john.omotani@ukaea.uk>
2023-01-09 23:18:50 +0000

    Revert "try running script with pytest to see if that makes a difference?"
    This reverts commit 0e59ca29bf3d6e58ed18634d97b1e21c349c0265.

--- elm-pb/3d-movie.py
+++ elm-pb/3d-movie.py
@@ -9,9 +9,6 @@ viewer](https://github.com/johnomotani/multigifview) helpful.
 
 from mayavi import mlab
 
-def test_fun():
-    print("foobar")
-
 #from argparse import ArgumentParser
 #import dask
 #from pathlib import Path
